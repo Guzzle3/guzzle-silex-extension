97d9b25f188ca6264ccd2a3a0f1a31920bd4738f
Michael Dowling <mtdowling@gmail.com>
2011-08-15 21:40:49 -0500

    Guzzle Silex extension

---
+++ GuzzleExtension.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Guzzle;
+
+use Silex\Application;
+use Silex\ExtensionInterface;
+use Guzzle\Service\ServiceBuilder;
+use Guzzle\Service\Client;
+
+/**
+ * Guzzle extension for Silex
+ *
+ * = Parameters:
+ *  guzzle.class_path: (optional) Path to where the Guzzle library is located.
+ *  guzzle.services: (optional) array|string|SimpleXMLElement Data describing
+ *      your web service clients.  You can pass the path to a file
+ *      (.xml|.js|.json), an array of data, or an instantiated SimpleXMLElement
+ *      containing configuration data.  See the Guzzle docs for more info.
+ *  guzzle.builder_cache: (optional) A Guzzle\Common\CacheAdapter object used
+ *      to cache the parsed configuration data.
+ *  guzzle.builder_ttl: (optional) How long to cache the parsed service data.
+ *  guzzle.builder_format: (optional) Pass the file extension (xml, js) when
+ *      using a file that does not use the standard file extension
+ *
+ * = Services:
+ *   guzzle: An instantiated Guzzle ServiceBuilder.
+ *   guzzle.client: A default Guzzle web service client using a dumb base URL.
+ *
+ * @link http://www.guzzlephp.org/docs/tour/using_services/#instantiating-web-service-clients-using-a-servicebuilder
+ * @link http://silex-project.org/doc/extensions.html
+ * @author Michael Dowling <michael@guzzlephp.org>
+ */
+class GuzzleExtension implements ExtensionInterface
+{
+    /**
+     * Register Guzzle with Silex
+     *
+     * @param Application $app Application to register with
+     */
+    public function register(Application $app)
+    {
+        // Register a Guzzle ServiceBuilder
+        $app['guzzle'] = $app->share(function () use ($app) {
+
+            if (!isset($app['guzzle.services'])) {
+                $builder = new ServiceBuilder(array());
+            } else {
+                $app['guzzle.builder_format'] = isset($app['guzzle.builder_format']) ?: false;
+                if (isset($app['guzzle.builder_cache'])) {
+                    $app['guzzle.builder_ttl'] = isset($app['guzzle.builder_ttl']) ?: 86400;
+                    $builder = ServiceBuilder::factory($app['guzzle.services'], $app['guzzle.builder_cache'], $app['guzzle.builder_ttl'], $app['guzzle.builder_format']);
+                } else {
+                    $builder = ServiceBuilder::factory($app['guzzle.services'], null, null, $app['guzzle.builder_format']);
+                }
+            }
+
+            return $builder;
+        });
+
+        // Register a simple Guzzle Client object (requires absolute URLs)
+        $app['guzzle.client'] = $app->share(function() use ($app) {
+            return new Client('/');
+        });
+
+        // Register the Guzzle namespace if guzzle.class_path is set
+        if (isset($app['guzzle.class_path'])) {
+            $app['autoloader']->registerNamespace('Guzzle', $app['guzzle.class_path']);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,59 @@
+Guzzle Silex extension
+======================
+
+The GuzzleExtension provides a Guzzle ServiceBuilder and default Client object through Michael Dowling’s Guzzle framework.  Guzzle is a PHP HTTP client and framework for building RESTful web service clients.
+
+You will need to [install a copy of Guzzle](http://www.guzzlephp.org/docs/tour/overview/) in order to use this extension.
+
+Parameters
+----------
+
+* guzzle.class_path: (optional) Path to where the Guzzle library is located.
+* guzzle.services: (optional) array|string|SimpleXMLElement Data describing your web service clients.  You can pass the path to a file (.xml|.js|.json), an array of data, or an instantiated SimpleXMLElement containing configuration data.  See the [Guzzle docs](http://www.guzzlephp.org/docs/tour/using_services/#instantiating-web-service-clients-using-a-servicebuilder) for more info.
+* guzzle.builder_cache: (optional) A Guzzle\Common\CacheAdapter object used to cache the parsed configuration data.
+* guzzle.builder_ttl: (optional) How long to cache the parsed configuration data.
+* guzzle.builder_format: (optional) Pass the file extension (xml, js) when using a file that does not use the standard file extension
+
+Services
+--------
+
+* guzzle: An instantiated Guzzle ServiceBuilder.
+* guzzle.client: A default Guzzle web service client using a dumb base URL.
+
+Registering
+-----------
+
+    require __DIR__ . '/../silex.phar';
+    require __DIR__ . '/../vendor/Guzzle/GuzzleExtension.php';
+
+    use Silex\Application;
+    use Guzzle\GuzzleExtension;
+
+    $app = new Application();
+
+    $app->register(new GuzzleExtension(), array(
+        'guzzle.services' => '/path/to/services.js',
+        'guzzle.class_path' => '/path/to/guzzle/src'
+    ));
+
+Example Usage
+-------------
+
+Using the instantiated ServiceBuilder:
+
+    // Get a command from your Amazon S3 client
+    $command = $app['guzzle']['s3']->getCommand('bucket.list_bucket');
+    $command->setBucket('mybucket');
+
+    $objects = $client->execute($command);
+    foreach ($objects as $object) {
+        echo "{$object['key']} {$object['size']}\n";
+    }
+
+Using the Guzzle client:
+
+    $response = $app['guzzle.client']->head('http://www.guzzlephp.org/)->send();
+
+More information
+----------------
+More information about Guzzle ServiceBuilders can be found at http://www.guzzlephp.org/docs/tour/using_services/#instantiating-web-service-clients-using-a-servicebuilder
